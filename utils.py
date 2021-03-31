958e00f1abc660dd9c1d610ae5f32f35d0c2a3f5
khalido <khalid.omar@gmail.com>
2021-03-31 17:27:50 +1100

    failed attempt

--- utils.py
+++ utils.py
@@ -1,22 +1,25 @@
 import numpy as np
 import requests
 
+
 def get_input(day=1, year=2015, split_on="\n"):
     """takes in the day, year and a string to split on"""
     try:
-        with open(f'../inputs/{year}/{day}.txt') as f:
+        with open(f"../inputs/{year}/{day}.txt") as f:
             data = f.read()
     except:
         print(f"Failed to load {year}/{day}.txt from disk, trying to get from github")
         url = f"https://github.com/khalido/adventofcode/raw/master/inputs/{year}/{day}.txt"
         data = requests.get(url).text
     return data
-    #return data.strip().split(split_on)
-        
+    # return data.strip().split(split_on)
+
+
 def printmd(txt="## testing"):
     """displays a text string as markdown"""
     display(Markdown(txt))
-    
+
+
 def rotate(arr, num):
     """shits items in arr by num"""
     return np.roll(arr, num)
